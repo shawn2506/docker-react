65e50ae816471f59d4a8bc7a70e9d64f2a3bc30f
shawn2506 <dsouzashawn62@yahoo.in>
2019-06-22 12:00:02 +0530

    changed app content

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          REACT JS
+          Changed App.js now
         </p>
         <a
           className="App-link"
